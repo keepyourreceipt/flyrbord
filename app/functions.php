5b5420fd616af10c2df5ef14b87b292d52a83bee
michaelkpeterman <michael@36creative.com>
2017-04-14 23:07:15 -0400

    Begin adding woocommerce support

--- app/functions.php
+++ app/functions.php
@@ -4,5 +4,13 @@
  */
 function include_hugoandlily_scripts() {
     wp_enqueue_script( 'hugoandlily-scripts', get_template_directory_uri() . '/js/main.js', array( 'jquery' ) );
+
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'include_hugoandlily_scripts' );
+
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
