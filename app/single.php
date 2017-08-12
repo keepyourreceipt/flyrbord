26474eafa232abac7c6515cd726e50f2f4ea8021
michaelkpeterman <michael@36creative.com>
2017-08-11 23:51:00 -0400

    Add comments form

--- app/single.php
+++ app/single.php
@@ -1,17 +1,49 @@
-<?php
-  get_header();
-  if( have_posts() ) {
-    while( have_posts() ) {
-      the_post();
-      ?>
-
-      <h2><?php the_title(); ?></h2>
-      <p><?php the_content(); ?></p>
-      <a href="<?php the_permalink(); ?>">Link: </a>
-
-
-      <?php
+<?php get_header(); ?>
+<div class="row">
+  <?php
+    $hase_sidebar = false;
+    if ( is_active_sidebar( 'blog_sidebar' ) ) {
+      $has_sidebar = true;
+      $posts_collection_classes = "col-sm-8";
+    } else {
+      $posts_collection_classes = "col-sm-10 col-sm-offset-1";
     }
-  }
-  get_footer();
-?>
+  ?>
+  <div class="<?php echo $posts_collection_classes; ?> post-content">
+    <?php
+    if( have_posts() ) {
+      while( have_posts() ) {
+        the_post();
+
+          // If post has thumbnail, display image
+          if ( has_post_thumbnail() ) {
+              the_post_thumbnail('large');
+          }
+        ?>
+        <h2><?php echo get_the_title(); ?></h2>
+        <p><?php echo get_the_content(); ?></p>
+        <?php
+      }
+    } ?>
+
+    <div class="row">
+      <div class="col-sm-12 post-comments">
+        <?php
+          // If comments open, show comments form
+          // NOTE: comments template modified from twentyseventeen theme
+          if ( comments_open() ) {
+            comments_template();
+          }
+        ?>
+      </div>
+    </div>
+
+  </div><!-- End single post content -->
+
+  <?php
+    // If Blog Sidebar has active widgets, include sidebar template
+    if ( $has_sidebar ) { get_sidebar( 'blog' ); }
+  ?>
+
+</div>
+<?php get_footer(); ?>
