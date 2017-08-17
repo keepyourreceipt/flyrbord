330118b7eeef89a4fe69766105fcff293314169f
Michael Peterman <michaelkpeterman@gmail.com>
2017-08-17 17:46:30 -0400

    Update woocommerce checkout and styles

--- app/functions.php
+++ app/functions.php
@@ -28,7 +28,37 @@ add_action( 'wp_enqueue_scripts', 'include_flyrbord_scripts' );
 
 add_image_size( 'post-listing', 900, 506, true );
 
+// Clear checkout floats
+function clearfix() {
+  echo "<div style='clear: both;'></div>";
+}
+add_action( 'woocommerce_after_checkout_form', 'clearfix' );
 
+// Wrap checkout columns in containing divs
+function wrap_woocommerce_checkout_form() {
+  echo "<div class='woocommerce-checkout-form-column col-sm-6'>";
+}
+add_action( 'woocommerce_checkout_before_customer_details', 'wrap_woocommerce_checkout_form' );
+add_action( 'woocommerce_checkout_after_customer_details', 'clearfix' );
+add_action( 'woocommerce_checkout_after_customer_details', 'output_closing_div_tag' );
+
+add_action( 'woocommerce_checkout_after_customer_details', 'wrap_woocommerce_checkout_form' );
+add_action( 'woocommerce_checkout_after_order_review', 'output_closing_div_tag' );
+
+// Wrap woocommerce checout form in containing div
+function start_checkout_form_wrap() {
+  echo "<div class='woocommerce-checkout-form row'>";
+}
+add_action( 'woocommerce_before_checkout_form', 'start_checkout_form_wrap' );
+add_action( 'woocommerce_after_checkout_form', 'output_closing_div_tag' );
+
+
+// Adjust login form column width
+function start_login_form_wrap() {
+  echo "<div class='col-sm-6 col-sm-offset-3 woocommerce-login-form'>";
+}
+add_action( 'woocommerce_before_customer_login_form', 'start_login_form_wrap' );
+add_action( 'woocommerce_after_customer_login_form', 'output_closing_div_tag' );
 
 function start_small_column_wrap() {
   echo "<div class='col-sm-10 col-sm-offset-1'>";
@@ -36,10 +66,10 @@ function start_small_column_wrap() {
 add_action( 'woocommerce_before_cart', 'start_small_column_wrap' );
 
 
-function end_small_column_wrap() {
+function output_closing_div_tag() {
   echo "</div>";
 }
-add_action( 'woocommerce_after_cart', 'end_small_column_wrap' );
+add_action( 'woocommerce_after_cart', 'output_closing_div_tag' );
 
 /**
  * Add Qoo Commerce support
