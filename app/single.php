9bdf15e7dc2f4b79bbc6c69cfb5c718f3cf34c39
michaelkpeterman <michael@36creative.com>
2017-08-19 17:08:16 -0400

    Update custom comments template structure

--- app/single.php
+++ app/single.php
@@ -7,36 +7,40 @@
           if ( has_post_thumbnail() ) {
               $featured_image_url = get_the_post_thumbnail_url(get_the_ID(),'large');
           }
-    ?>
-    <div class="row">
-      <div class="col-sm-12 post-header">
-        <div class="post-header" style="background-image: url(<?php echo $featured_image_url; ?>)">
-          <div class="container">
-            <div class="col-sm-10 col-sm-offset-1 content">
-              <h2><?php echo get_the_title(); ?></h2>
-              <h5><?php echo get_the_date(); ?></h5>
-            </div>
-          </div>
-        </div>
-      </div><!-- /.post-header -->
+          ?>
+          <div class="row">
+            <div class="col-sm-12 post-header">
+              <div class="post-header" style="background-image: url(<?php echo $featured_image_url; ?>)">
+                <div class="container">
+                  <div class="col-sm-10 col-sm-offset-1 content">
+                    <h2><?php echo get_the_title(); ?></h2>
+                    <h5><?php echo get_the_date(); ?></h5>
+                  </div>
+                </div>
+              </div>
+            </div><!-- /.post-header -->
 
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-10 col-sm-offset-1 post-content">
-          <div class="col-sm-12 post-comments">
-            <?php the_content(); ?>
-            <?php
-              // If comments open, show comments form
-              // NOTE: comments template modified from twentyseventeen theme
-              if ( comments_open() ) {
-                comments_template();
-              }
-            ?>
-        </div>
-      </div>
-    </div>
-  <?php
-  }
-} ?>
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-10 col-sm-offset-1 post-content">
+                  <?php the_content(); ?>
+                </div>
+              </div>
+            </div> <!-- /.post-comments -->
+
+            <?php if ( comments_open() ) { ?>
+              <div class="container">
+                <div class="row">
+                  <div class="col-sm-10 col-sm-offset-1 post-comments">
+                    <?php comments_template(); ?>
+                  </div>
+                </div>
+              </div>
+            <?php } ?>
+
+          </div><!-- /.row -->
+      <?php
+      }
+    } ?>
 
 <?php get_footer(); ?>
