668e08978c88d0d977901f711845f468e59776ef
michaelkpeterman <michael@36creative.com>
2017-08-30 19:32:10 -0400

    Adjust top bar layout classes

--- app/inc/template-partials/top-bar.php
+++ app/inc/template-partials/top-bar.php
@@ -4,13 +4,13 @@
       <div class="row">
 
         <?php if ( get_theme_mod( 'announcement_text', '' ) ) { ?>
-          <div class="col-xs-12 col-sm-12 col-md-8 announcement-text">
+          <div class="col-xs-12 col-sm-12 col-md-6 announcement-text">
             <p><?php echo get_theme_mod( 'announcement_text', '' ); ?></p>
           </div>
         <?php } ?>
 
         <?php if ( get_theme_mod( 'phone_number', '' ) || get_theme_mod( 'public_email', '' ) ) { ?>
-          <div class="col-xs-12 col-sm-4 contact-info hidden-xs hidden-sm">
+          <div class="col-xs-12 col-sm-6 contact-info hidden-xs hidden-sm">
             <?php if ( get_theme_mod( 'phone_number', '' ) ) { ?>
               <span class="phone-number">Phone: <?php echo get_theme_mod( 'phone_number', '' ); ?></span>
             <?php } ?>
